4e96503e185bae71e2508e4ecfea1243220754bf
marudaga <83997563+marudaga@users.noreply.github.com>
2021-07-20 22:40:55 +0900

    3.2

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 const  PORT =4000;
 const app = express();
-//
-const handleListening =() => console.log("Server listening on port 4000");
+const handleHome= () => console.log("Somebody is trying to go home");
+
+app.get("/", handleHome);
+/*사용자가 get requetst하는게 아니라 브라우저가 get request 하는거다
+get request에는 route가 있다 어디로 가려는지  ex) /, home/
+*/
+
+const handleListening =() => console.log(`Server listening on port http://locahost:${PORT}`);
 app.listen(PORT, handleListening);
-/*http 서버랑 interactive 하는 방법
-http request란 웹사이트에 접속하는 방법 or 서버에 정보를 보내는 방법 
-GET은 http methods이고 get that page라는 뜻을 의미함
-'/'는  root나 서버의 첫페이지를 뜻한다
+/* app 생성, 설정 외부로 개방
 */
