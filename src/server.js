ea40194fed31e3129f990929c7dd8b6cf8b019ad
marudaga <83997563+marudaga@users.noreply.github.com>
2021-07-20 21:21:59 +0900

    3.1

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 import express from "express";
 const  PORT =4000;
 const app = express();
-//express 앱을 실행  request란  www.google.com는 google.com에 request를 보낸것임 
-// server은 listen and response한다
+//
 const handleListening =() => console.log("Server listening on port 4000");
 app.listen(PORT, handleListening);
-/*callback 함수란 다른 함수에 파라미터로 전달되는 함수
-port 큰숫자는 대부분 비어있다
-localhost:4000
-ctrl +c 작업종료
+/*http 서버랑 interactive 하는 방법
+http request란 웹사이트에 접속하는 방법 or 서버에 정보를 보내는 방법 
+GET은 http methods이고 get that page라는 뜻을 의미함
+'/'는  root나 서버의 첫페이지를 뜻한다
 */
