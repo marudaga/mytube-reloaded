e6d4b37cf7239b3d459f019f3a0148f6be1dae52
marudaga <83997563+marudaga@users.noreply.github.com>
2021-07-21 22:26:24 +0900

    3.6

--- src/server.js
+++ src/server.js
@@ -2,26 +2,39 @@ import express from "express";
 const  PORT =4000;
 const app = express();
 
-const gassipMiddleware = (req, res, next) => {
-    console.log("middle");
+const logger  = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
     next();
-}
-
-const handleHome= (req, res) => 
-{
-    return res.end();
 };
 
-app.get("/", gassipMiddleware, handleHome);
+const  privateMiddleware = (req, res,next) => {
+    const url = req.url;
+    if(url === "/protected"){
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    console.log("allowed, you may continue");
+    next();
+};
+
+const handleHome = (req, res) => 
+{
+    return res.send("i love middleware");
+};
+const handleProtected = (req, res) => 
+{
+    return res.send("welcome to the private lounge");
+};
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 /* 
-브라우저가 request하면 서버가 response한다
-app.get은 브라우저가 홈페이지 get하는 것
-middleware = middle software middleware은 request와 response사이에 있다
-middleware = handler이고  handler를 controller라고 한다
-controller은 (req, res, next)로  next가 하나더 있다.
-작동순서?  app.get / url를 얻고 그다음 express에서 gassip->next가
- handleHome실행하는데 마지막 controller라서 next가 필요없다
- send("comment"),send("<h1>comment</h1>") 작성양식
+app.use 를 먼저쓰고 get을 써야 하위에 get에 전부 적용됨
+app.use(logger)
+app.get("/", handleHome);
+===
+app.use();
+app.get("/", logger, handleHome); 
 */
 
 const handleListening =() => console.log(`Server listening on port http://locahost:${PORT}`);
