089212597333842a955e9ef623779b8ea10e5a11
marudaga <83997563+marudaga@users.noreply.github.com>
2021-07-23 21:54:35 +0900

    3.10

--- src/server.js
+++ src/server.js
@@ -1,43 +1,39 @@
 import express from "express";
-const  PORT =4000;
+
 const app = express();
 
-const logger  = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
+const urlLogger = (req, res, next) => {
+  const url = req.url;
+  console.log(`Path: ${url}`);
+  next();
+};
+
+const timeLogger = (req, res, next) => {
+  console.log(`Time: ${new Date()}`);
+  next();
+};
+const securityLogger = (req, res, next) => {
+  const check = req.protocol;
+  if (check === "http") {
+    console.log(`Insecure`);
     next();
-};
-
-const  privateMiddleware = (req, res,next) => {
-    const url = req.url;
-    if(url === "/protected"){
-        return res.send("<h1>Not Allowed</h1>");
-    }
-    console.log("allowed, you may continue");
+  } else {
+    console.log(`secure`);
     next();
+  }
+};
+const protectorMiddleware = (req, res, next) => {
+  if (req.url === "protected") {
+    next();
+  }
 };
 
-const handleHome = (req, res) => 
-{
-    return res.send("i love middleware");
-};
-const handleProtected = (req, res) => 
-{
-    return res.send("welcome to the private lounge");
-};
-app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
-/* 
-app.use 를 먼저쓰고 get을 써야 하위에 get에 전부 적용됨
-app.use(logger)
-app.get("/", handleHome);
-===
-app.use();
-app.get("/", logger, handleHome); 
-*/
+app.use(urlLogger);
+app.use(timeLogger);
+app.use(securityLogger);
+app.use(protectorMiddleware);
+app.get("/", (req, res) => res.send("<h1>Home</h1>"));
+app.get("/protected", (req, res) => res.send("<h1>Protected</h1>"));
 
-const handleListening =() => console.log(`Server listening on port http://locahost:${PORT}`);
-app.listen(PORT, handleListening);
-/* 
-*/
+// Codesandbox gives us a PORT :)
+app.listen(PORT, () => `Listening!✅`);
