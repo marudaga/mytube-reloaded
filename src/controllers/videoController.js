2a60f437ebcffc778c592cdb91e942b664a36fb5
marudaga <83997563+marudaga@users.noreply.github.com>
2021-07-28 20:48:06 +0900

    5.7 not integrated with before lecture
    just5.7 lecture example with fakeUser

---
+++ src/controllers/videoController.js
@@ -0,0 +1,14 @@
+
+const fakeUser = {
+    username:"Nicolas",
+    loggedIn:false,
+};
+
+export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req, res) => {
+  return res.send("Delete Video");
+};
